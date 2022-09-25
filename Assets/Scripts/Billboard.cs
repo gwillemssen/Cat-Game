e2d9cde88e464ed638644b90e88412f5c6c68913
DrDezmund <dezjames123@gmail.com>
2022-09-25 14:06:36 -0500

    AI Prototype

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour
+{
+    public static Transform cam;
+    
+    private Vector3 freeRotation = Vector3.one;
+    private Vector3 eangles = Vector3.zero;
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        }
+    }
+
+    void LateUpdate()
+    {
+        this.transform.LookAt(cam);
+        transform.Rotate(0, 180, 0);
+        eangles = transform.eulerAngles;
+        eangles.x *= freeRotation.x;
+        eangles.y *= freeRotation.y;
+        eangles.z *= freeRotation.z;
+        transform.eulerAngles = eangles;
+    }
+}
