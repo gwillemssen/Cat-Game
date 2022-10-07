add5e6ad8025fefe9e86fee8220c2cb33c5abd6a
Dez Boyle <dezjames123@gmail.com>
2022-10-07 13:44:58 -0500

    Moved GameManager.cs and LevelManager.cs to /Scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
+    public static GameManager instance { get; private set; };
 
     void Awake()
     {
