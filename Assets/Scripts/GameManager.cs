7abb4badd4110c527b34178e7ddb1f05a85e2acc
Dez Boyle <dezjames123@gmail.com>
2022-10-07 13:45:47 -0500

    again

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance { get; private set; };
+    public static GameManager instance { get; private set; }
 
     void Awake()
     {
