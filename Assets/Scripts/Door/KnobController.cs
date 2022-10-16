ae09300eb5d0518c115f2f0436a87f9d121411f8
Naikym <mepower2001@gmail.com>
2022-10-16 09:29:22 -0500

    Fixed a few errors for GreyboxTest1
    It was missing the A* gameobject and a collider for one of the hinges.

--- Assets/Scripts/Door/KnobController.cs
+++ Assets/Scripts/Door/KnobController.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class KnobController : Interactable
 {
     private DoorAnimController parent;
-
+   
     private void Start()
     {
         parent = transform.parent.parent.GetComponent<DoorAnimController>();
+       
     }
 
     public override void InteractClick(FirstPersonController controller)
