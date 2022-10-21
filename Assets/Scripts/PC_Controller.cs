3cf5af79e27c512b4ad1185e2cf70763cfb234e5
Brian R <ryztiq@gmail.com>
2022-10-21 15:29:30 -0500

    Added LeanTween And fixed the switch/pc/stuff

--- Assets/Scripts/PC_Controller.cs
+++ Assets/Scripts/PC_Controller.cs
@@ -11,16 +11,33 @@ public class PC_Controller : Interactable
     public GameObject objPC;
     private Renderer pcRenderer;
     public Texture pcTex;
+    private Material mat;
+    public bool on;
 
     private void Start()
     {
+
         pcRenderer = objPC.GetComponent<Renderer>();
+        mat = Instantiate(pcRenderer.material);
+        pcRenderer.material = mat;
     }
     public override void InteractClick(FirstPersonController controller)
     {
-        AudioManager.instance.Play("PC Startup");
-        AudioManager.instance.Play("PC Distraction");
-        pcRenderer.material.SetTexture("PC_ON_BaseColor", pcTex);
-        
+        if (on)
+        {
+            mat.SetFloat(Shader.PropertyToID("_PowerBool"),0);
+            on = !on;
+            AudioManager.instance.Stop("PC Startup");
+            AudioManager.instance.Stop("PC Distraction");
+        }
+        else
+        {
+            mat.SetFloat(Shader.PropertyToID("_PowerBool"),1);
+            on = !on;
+            AudioManager.instance.Play("PC Startup");
+            AudioManager.instance.Play("PC Distraction");
+        }
+
+
     }
 }
