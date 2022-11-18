608f606f3dc9a9df3fc8d58e885bc8a57cecae26
Dez Boyle <dezjames123@gmail.com>
2022-11-18 14:41:54 -0600

    slightly broken release

--- Assets/Scripts/Door/DoorAnimController.cs
+++ Assets/Scripts/Door/DoorAnimController.cs
@@ -14,27 +14,42 @@ public class DoorAnimController : MonoBehaviour
    
     public float Cooldown;
 
+    private Quaternion startRot;
+    private Quaternion endRot;
+    private Quaternion target;
+
     // Start is called before the first frame update
     void Start()
     {
         doorHinge = transform.GetChild(3).GetChild(0).gameObject;
         doorCollider = doorHinge.GetComponent<Collider>();
+        startRot = doorHinge.transform.rotation;
+        endRot = Quaternion.Euler(new Vector3(doorHinge.transform.rotation.eulerAngles.x, doorHinge.transform.rotation.eulerAngles.y, doorHinge.transform.rotation.eulerAngles.z + 100f));
+        target = startRot;
     }
 
     public void Open(bool open)
     {
         if (open)
         {
-            doorHinge.LeanRotateY(-90,0.2f);
+            //doorHinge.LeanRotateY(-90,0.2f);
+            target = endRot;
+            doorCollider.enabled = false;
         }
         else
         {
-            doorHinge.LeanRotateY(0,0.2f);
+            //doorHinge.LeanRotateY(0,0.2f);
+            target = startRot;
+            doorCollider.enabled = true;
         }
-
     }
-    
-    
+
+    private void Update()
+    {
+        doorHinge.transform.rotation = Quaternion.Lerp(doorHinge.transform.rotation, target, Time.deltaTime * 5f);
+    }
+
+
 
 
 }
