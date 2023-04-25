f381948f4966ff1ba54d2d8d5e8bc1e9e80962f1
Brian R <63207674+Ryztiq@users.noreply.github.com>
2023-04-25 15:56:08 -0500

    Updated switch script to make all lights toggle based on their own values (relied on the values of the lightswitch previously.

--- Assets/Scripts/SwitchController.cs
+++ Assets/Scripts/SwitchController.cs
@@ -18,24 +18,38 @@ public class SwitchController : Interactable
     // 0: on
     // 1: off
     public AudioSource lightSwitchPlayer;
-    void Start()
-    {
-        SwitchLoad();
-    }
+    private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
 
+    private void Start()
+    {
+        //initialize the emission colors of each light.
+        foreach (LightInfo light in lights) if(light.meshRenderer)light.meshColor = light.meshRenderer.material.GetColor(EmissionColor);
+    }
     void ToggleLight()
     {
-        if(LeanTween.isTweening(switchBone.gameObject)) LeanTween.cancel(switchBone.gameObject);
-        foreach (var light in lights.Where(light=> light != null))
-            light.light.enabled = on;
-
-        foreach (var lightMesh in lights.Where(lightMesh => lightMesh != null))
-            lightMesh.meshRenderer.material.SetColor("_EmissionColor", on ? Color.yellow : Color.black);
-
-        LeanTween.value(switchBone.gameObject, 0, 1, 0.5f).setOnUpdate((float val) =>
+        foreach (var light in lights)
         {
-            switchBone.transform.localRotation = Quaternion.Lerp(switchBone.transform.localRotation, Quaternion.Euler(on?onPos:offPos), val);
-        });
+            //toggle light if it exists
+            if (light.light) light.light.enabled = !light.light.enabled;
+
+            //toggle emission if the entry exists
+            if (!light.meshRenderer) continue;
+            Color lightColor;
+            lightColor = light.meshRenderer.material.GetColor(EmissionColor);
+                
+            if(LeanTween.isTweening(light.meshRenderer.gameObject)) LeanTween.cancel(light.meshRenderer.gameObject);
+            LeanTween.value(light.meshRenderer.gameObject, 0, 1, 0.05f).setOnUpdate(val =>
+                light.meshRenderer.material.SetColor(EmissionColor,
+                    Color.Lerp(lightColor,
+                        light.light.enabled ? light.meshColor : Color.black,val)));
+        }
+
+        //Animate Lightswitch
+        if(LeanTween.isTweening(switchBone.gameObject)) LeanTween.cancel(switchBone.gameObject);
+        LeanTween.value(switchBone.gameObject, 0, 1, 0.5f).setOnUpdate(val =>
+            switchBone.transform.localRotation = Quaternion.Lerp(switchBone.transform.localRotation,
+                Quaternion.Euler(on ? onPos : offPos), val));
+        //Play Lightswitch Sound
         lightSwitchPlayer.PlayOneShot(on ? sounds[0] : sounds[1]);
     }
 
@@ -46,18 +60,11 @@ public class SwitchController : Interactable
         ToggleLight();
     }
 
-    private void SwitchLoad()
-    {
-        //initialization
-        lightSwitchPlayer = GetComponent<AudioSource>();
-        // switchBone = transform.GetChild(2).GetChild(0).gameObject;
-        //Debug.Log($"{gameObject.name} Initialized");
-    }
-
     [Serializable]
     public class LightInfo
     {
         public Light light;
         public MeshRenderer meshRenderer;
+        [HideInInspector]public Color meshColor;
     }
 }
