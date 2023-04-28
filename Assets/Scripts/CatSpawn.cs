9802428dcfe3e93126d4b56fe41602b58057201f
Lilysium <Lilyh3738@gmail.com>
2023-04-28 15:25:11 -0500

    Cat Spawns Fixed!

--- Assets/Scripts/CatSpawn.cs
+++ Assets/Scripts/CatSpawn.cs
@@ -14,10 +14,11 @@ public class CatSpawn : MonoBehaviour
 
     public List<GameObject> catPrefabs;
 
-    public List<Transform> catPos;
+    private List<Transform> catPos;
     // Start is called before the first frame update
     void Start()
     {
+        catPos= new List<Transform>(); 
         foreach (var cat in CatWaypoints)
         {
             catPos.Add(cat.catPosition);
@@ -31,18 +32,21 @@ public class CatSpawn : MonoBehaviour
         List<Material> tempMats = new List<Material>(catMats);
         List<Transform> tempPos = new List<Transform>(catPos);
         List<GameObject> tempPrefabs = new List<GameObject>();
-        foreach(var cat in CatWaypoints)
-        {
-            if (cat.UseSittingCat) { tempPrefabs.Add(tempPrefabs[0]); }
-            if(cat.UseLayingCat) { tempPrefabs.Add(tempPrefabs[1]); }
-            
-        }
+        
         List<Material> tempEyeMats = new List<Material>(catEyeMats);
         for (int i = 0; i < catMats.Count; i++)
         {
+            foreach (var cats in CatWaypoints)
+            {
+                if (cats.UseSittingCat) { tempPrefabs.Add(catPrefabs[0]);}
+                if (cats.UseLayingCat) { tempPrefabs.Add(catPrefabs[1]);}
+
+            }
             int matIndex = UnityEngine.Random.Range(0, tempMats.Count);
             int posIndex = UnityEngine.Random.Range(0, tempPos.Count);
-            GameObject cat = Instantiate(tempPrefabs[UnityEngine.Random.Range(0,tempPrefabs.Count -1)], tempPos[posIndex].position, tempPos[posIndex].rotation);
+            int prefabIndex = UnityEngine.Random.Range(0, tempPrefabs.Count);
+            if(prefabIndex < 0) { prefabIndex= 0; } 
+            GameObject cat = Instantiate(tempPrefabs[prefabIndex], tempPos[posIndex].position, tempPos[posIndex].rotation);
             cat.transform.GetChild(3).GetComponent<Renderer>().material = tempMats[matIndex];
             cat.transform.GetChild(1).GetComponent<Renderer>().material = tempEyeMats[matIndex];
             tempMats.RemoveAt(matIndex);
