4e9b9373a337a65721c091e4497100c45e7f5da7
Lily Hoekstra <Lilyh3738@gmail.com>
2023-05-02 23:00:38 -0500

    candles and fire

--- Assets/Scripts/Bookshelf.cs
+++ Assets/Scripts/Bookshelf.cs
@@ -4,35 +4,24 @@ using UnityEngine;
 
 public class Bookshelf : Interactable
 {
-    public GameObject BookshelfL;
-    public GameObject BookshelfR;
-    // Start is called before the first frame update
+    public GameObject BookshelfL, BookshelfR, CandlesParent;
+
     void Start()
     {
-        
+        CandlesParent.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
     public override void InteractWith(FirstPersonController controller, Interactable withInteractable)
     {
-
-
         if (withInteractable.name == RequiredItemToViewInteraction) 
         {
-            LeanTween.moveLocalZ(BookshelfL, 2.6f, 2.5f).setEaseLinear();
+            LeanTween.moveLocalZ(BookshelfL, 2.5f, 2.5f).setEaseLinear();
             LeanTween.moveLocalZ(BookshelfR, -7f, 2.5f).setEaseLinear();
             transform.GetChild(0).gameObject.SetActive(false);
             transform.GetChild(1).gameObject.SetActive(true);
+            CandlesParent.SetActive(true);
             controller.Interaction.DestroyPickup();
         }
-
-
     }
 
 }
---
+++ Assets/Scripts/SummoningCircle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummoningCircle : MonoBehaviour
+{
+    public List<GameObject> Candles = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+
+}
