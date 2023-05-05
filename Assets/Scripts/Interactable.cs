20ea1072dbd692970ea7b7ddc2de01527210ba01
gwillemssen <57201850+gwillemssen@users.noreply.github.com>
2023-05-04 23:25:00 -0500

    began toilet flush progress

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -86,4 +86,9 @@ public class Interactable : MonoBehaviour
         player.Play(sounds[Random.Range(0, sounds.Count)]);
     }
 
+    public void PlayAnimation(Animation anim )
+    {
+        anim.Stop();
+        anim.Play();
+    }
 }
